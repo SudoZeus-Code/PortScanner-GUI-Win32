054df6b36c5bdfed6afef14fc07d9c09dd901db4
sudozeus <sudozeus@protonmail.ch>
2025-04-07 20:52:14 -0700

    added png, switched to a thread pool

--- src/scanner.rs
+++ src/scanner.rs
@@ -4,6 +4,7 @@ use std::net::{TcpStream, SocketAddr};
 use std::time::Duration;
 use std::thread;
 use std::sync::mpsc;
+use rayon::prelude::*;
 
 // not needed as im calling mpsc above
 // nope i actually need this
@@ -15,7 +16,8 @@ pub fn run_scan(target: String, tx: Sender<String>) {
     //let target = "127.0.0.1";
     let start_port = 1;
     let end_port = 1024;    
-    let timeout = Duration::from_millis(10);
+    // increased timeout to 100
+    let timeout = Duration::from_millis(100);
 
     // print this to the console?
     //println!("Scanning {} on ports {}-{}", target, start_port, end_port);
@@ -24,27 +26,30 @@ pub fn run_scan(target: String, tx: Sender<String>) {
     // channel to communicate between the threads we open
     //let (tx, rx) = mpsc::channel::<String>(); // changed from mpsc::channel() to the String equivalent 
 
-    for port in start_port..=end_port {
+    //replacing "for loop" with a thread pool
+    (1..=1024).into_par_iter().for_each(|port| {
 
         let tx = tx.clone();
 
         let target = target.to_string();
 
-        thread::spawn(move || {
-           let address = format!("{}:{}", target, port);
-           if let Ok(socket) = address.parse::<SocketAddr>() {
+        let address = format!("{}:{}", target, port);
+        if let Ok(socket) = address.parse::<SocketAddr>() {
 
-               if TcpStream::connect_timeout(&socket, timeout).is_ok() {
-                    let msg = format!("{}: open\r\n", port);
-                    // DEBUG
-                    //println!("{}",msg);
-                    let _ = tx.send(msg);
-               }
+            if TcpStream::connect_timeout(&socket, timeout).is_ok() {
+                let msg = format!("{}: open\r\n", port);
+                // DEBUG
+                //println!("{}",msg);
+                let _ = tx.send(msg);
+            }
 
-           }
+        }
 
-        });
+        
     
-    }
+    });
+
+
+
 
 }
